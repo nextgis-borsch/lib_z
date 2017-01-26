2f9a5e8dcea25aeb673589ef0e7b508b432caf70
alisovenko <alexander.lisovenko@gmail.com>
2017-01-26 12:45:55 +0400

    Move version checking to utils.cmake. Generate version.str file.

--- cmake/util.cmake
+++ cmake/util.cmake
@@ -24,12 +24,29 @@
 # DEALINGS IN THE SOFTWARE.
 ################################################################################
 
+function(check_version version)
+
+    # parse the full version number from zlib.h and include in ZLIB_FULL_VERSION
+    file(READ ${CMAKE_CURRENT_SOURCE_DIR}/zlib.h _zlib_h_contents)
+    string(REGEX REPLACE ".*#define[ \t]+ZLIB_VERSION[ \t]+\"([-0-9A-Za-z.]+)\".*"
+    "\\1" ZLIB_FULL_VERSION ${_zlib_h_contents})
+
+    set(${version} ${ZLIB_FULL_VERSION} PARENT_SCOPE)
+
+    # Store version string in file for installer needs
+    file(TIMESTAMP ${CMAKE_CURRENT_SOURCE_DIR}/zlib.h VERSION_DATETIME "%Y-%m-%d %H:%M:%S" UTC)
+    file(WRITE ${CMAKE_BINARY_DIR}/version.str "${ZLIB_FULL_VERSION}\n${VERSION_DATETIME}")
+
+endfunction(check_version)
+
+
 function(report_version name ver)
 
     string(ASCII 27 Esc)
     set(BoldYellow  "${Esc}[1;33m")
     set(ColourReset "${Esc}[m")
-        
+
     message(STATUS "${BoldYellow}${name} version ${ver}${ColourReset}")
+
+endfunction()
     
-endfunction()    
